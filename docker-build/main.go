1b6a83220031fa923d0f3be4456bb532b6cc0840
Matthias Diester <matthias.diester@de.ibm.com>
2023-12-15 16:16:32 +0100

    Refactor

--- docker-build/main.go
+++ docker-build/main.go
@@ -7,21 +7,27 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
+	"strconv"
 	"syscall"
 )
 
-const port = 8080
-
 func main() {
 	ctx := context.Background()
-
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+
+	port := 8080
+	if strValue, ok := os.LookupEnv("PORT"); ok {
+		if intValue, err := strconv.Atoi(strValue); err == nil {
+			port = intValue
+		}
+	}
 
 	srv := &http.Server{Addr: fmt.Sprintf(":%d", port)}
 	go func() {
 		http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-			fmt.Fprintf(w, "Hello, World!")
+			fmt.Fprintf(w, "Hello, World! I am using %s by the way.", runtime.Version())
 		})
 
 		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
